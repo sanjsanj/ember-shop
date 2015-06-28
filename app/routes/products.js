3265f6cc889c6248696d9c7458c85622432c71dd
sanjsanj <sanjsanj@hotmail.com>
2015-06-28 19:42:24 +0100

    refactor header into a partial

--- app/routes/products.js
+++ app/routes/products.js
@@ -6,7 +6,6 @@ export default Ember.Route.extend({
   },
   actions: {
     addToCart: function(product){
-      // var products = this.store.find('product');
       var productStock = product.get('stock');
       product.set('stock', productStock - 1);
       console.log(product.get('stock'));
