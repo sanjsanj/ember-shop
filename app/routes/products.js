e5c2be5c2da223bb8c8e4a67632ed34cf2561d8f
sanjsanj <sanjsanj@hotmail.com>
2015-06-28 19:21:19 +0100

    buy button decrements stock

--- app/routes/products.js
+++ app/routes/products.js
@@ -6,7 +6,10 @@ export default Ember.Route.extend({
   },
   actions: {
     addToCart: function(product){
-      alert(product.get('price'));
+      // var products = this.store.find('product');
+      var productStock = product.get('stock');
+      product.set('stock', productStock - 1);
+      console.log(product.get('stock'));
     }
   }
 });
