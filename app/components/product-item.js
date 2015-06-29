d57e2b9c1ca35d6d4fa6f2d9c14f013e11590813
sanjsanj <sanjsanj@hotmail.com>
2015-06-29 11:54:44 +0100

    divest product-item actions to its component

--- app/components/product-item.js
+++ app/components/product-item.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+  actions: {
+    addToCart: function(product){
+      var productStock = product.get('stock');
+      product.set('stock', productStock - 1);
+      console.log(product.get('stock'));
+    }
+}});
--- app/routes/products.js
+++ app/routes/products.js
@@ -3,12 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     return this.store.find('product');
-  },
-  actions: {
-    addToCart: function(product){
-      var productStock = product.get('stock');
-      product.set('stock', productStock - 1);
-      console.log(product.get('stock'));
-    }
   }
 });
